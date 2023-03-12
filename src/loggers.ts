041b3307c4752040c35123576d31c15e0dfc0c92
Asif <asif.javed.bangash@gmail.com>
2023-03-12 16:44:56 +1100

    log-file-path-changed

--- src/loggers.ts
+++ src/loggers.ts
@@ -5,7 +5,7 @@ export const infoLogger = winston.createLogger({
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/info.log' }),
+    new winston.transports.File({ filename: 'info.log' }),
   ],
 })
 
@@ -14,6 +14,6 @@ export const errorLogger = winston.createLogger({
   format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'logs/error.log' }),
+    new winston.transports.File({ filename: 'error.log' }),
   ],
 })
