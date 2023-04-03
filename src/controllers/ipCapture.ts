4e4f2f7bae1dd0447f33a052abbace19d0b0817b
Asif <asif.javed.bangash@gmail.com>
2023-04-03 17:41:19 +1000

    fix

--- src/controllers/ipCapture.ts
+++ src/controllers/ipCapture.ts
@@ -5,12 +5,15 @@ const requestIp = require('request-ip')
 export const getClientIp = (request: Request): string => {
   let clientIp = requestIp.getClientIp(request)
 
+  console.log('This is clientIp from request.getClientIp: ' + clientIp)
+
   // Check for a forwarded IP address in the X-Forwarded-For header
   const forwardedFor = request.headers['x-forwarded-for']
   if (typeof forwardedFor === 'string') {
     // X-Forwarded-For can contain a comma-separated list of IP addresses
     // The client's IP address is typically the first address in the list
     clientIp = forwardedFor.split(',')[0]
+    console.log('This is clientIp from x-forwarded-for header: ' + clientIp)
   }
 
   return clientIp
