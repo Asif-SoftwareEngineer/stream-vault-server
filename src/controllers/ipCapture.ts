6ee50c00dc22ad3fc3eb532bac0782ebb36a5b5f
Asif <asif.javed.bangash@gmail.com>
2023-04-03 19:01:03 +1000

    FIX

--- src/controllers/ipCapture.ts
+++ src/controllers/ipCapture.ts
@@ -13,6 +13,7 @@ export const getClientIp = (request: Request): string => {
     // X-Forwarded-For can contain a comma-separated list of IP addresses
     // The client's IP address is typically the first address in the list
     clientIp = forwardedFor.split(',')[0]
+    console.log('LIst of client ips within x-forwarded-for header: ' + forwardedFor)
     console.log('This is clientIp from x-forwarded-for header: ' + clientIp)
   }
 
