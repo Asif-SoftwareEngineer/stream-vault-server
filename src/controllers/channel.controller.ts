1d8c5063d5727c30a73766526638a9dd32fd9518
Asif <asif.javed.bangash@gmail.com>
2023-06-27 16:37:10 +1000

    new-channel-screen-complete

--- src/controllers/channel.controller.ts
+++ src/controllers/channel.controller.ts
@@ -2,25 +2,42 @@ import { NextFunction, Request, Response } from 'express'
 
 import { userModel } from '../models/user'
 
-export function checkForDuplicateChannels(req: Request, res: Response, next: NextFunction) {
+export function checkForDuplicateChannels(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   userModel.aggregate(
     [
       { $unwind: '$channels' },
-      { $group: { _id: '$channels.name', count: { $sum: 1 } } },
-      { $match: { count: { $gt: 0 } } },
+      {
+        $group: {
+          _id: { $toLower: '$channels.name' },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $match: {
+          count: { $gt: 0 },
+          _id: { $regex: new RegExp(req.body.name.toLowerCase(), 'i') },
+        },
+      },
     ],
     function (err, result) {
       if (err) {
-        res.status(400).send({
+        console.log(err)
+        return res.status(400).send({
           errorMessage: `Error happened while checking duplicate record for channel name`,
         })
       } else if (result) {
-        let isChannelNameExists = result.find(({ _id }) => _id === req.body.name)
+        let isChannelNameExists = result.find(
+          ({ _id }) => _id === req.body.name.toLowerCase()
+        )
 
         if (isChannelNameExists) {
           return res.status(409).send({
             status: 409,
-            errorMessage: `Duplicate record found! Channel Name:[${isChannelNameExists._id}] already exists!`,
+            errorMessage: `Duplicate record found! Channel Name [${isChannelNameExists._id}] already exists!`,
           })
         } else {
           return next()
